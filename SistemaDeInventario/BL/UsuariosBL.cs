6ce22e5b498aeb352254c5045de275aba3f59d0b
MBERN001 <40253706+MBERN001@users.noreply.github.com>
2019-09-12 23:56:23 -0600

    Add files via upload

---
+++ SistemaDeInventario/BL/UsuariosBL.cs
@@ -0,0 +1,65 @@
+﻿using SistemaDeInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeInventario.BL
+{
+   public class UsuariosBL
+    {
+
+        public BindingList<Usuario> ListadeUsuarios { get; set; }
+
+        public UsuariosBL()
+        {
+            ListadeUsuarios = new BindingList<Usuario>();
+            CargarDatosdePrueba();         
+        }
+        private void CargarDatosdePrueba()
+        {
+            var usuarioAdmin = new Usuario(1, "Milton", "478");
+            usuarioAdmin.PuedeVerClientes = true;
+            usuarioAdmin.PuedeVerCompras = true;
+            usuarioAdmin.PuedeVerFacturas = true;
+            usuarioAdmin.PuedeVerProductos = true;
+            usuarioAdmin.PuedeVerProveedores = true;
+            usuarioAdmin.PuedeVerReportes = true;
+
+            var usuarioAdmin2 = new Usuario(2, "Jorge", "124");
+            usuarioAdmin.PuedeVerClientes = true;
+            usuarioAdmin.PuedeVerCompras = true;
+            usuarioAdmin.PuedeVerFacturas = true;
+            usuarioAdmin.PuedeVerProductos = true;
+            usuarioAdmin.PuedeVerProveedores = true;
+            usuarioAdmin.PuedeVerReportes = true;
+
+            var usuarioAdmin3 = new Usuario(3, "Roberto", "321");
+            usuarioAdmin.PuedeVerClientes = true;
+            usuarioAdmin.PuedeVerCompras = true;
+            usuarioAdmin.PuedeVerFacturas = true;
+            usuarioAdmin.PuedeVerProductos = true;
+            usuarioAdmin.PuedeVerProveedores = true;
+            usuarioAdmin.PuedeVerReportes = true;
+
+            ListadeUsuarios.Add(usuarioAdmin);
+            ListadeUsuarios.Add(usuarioAdmin2);
+            ListadeUsuarios.Add(usuarioAdmin3);
+
+        }
+        public Usuario Autenticar(string usuario, string contrasena)
+        {
+            foreach (var usuario in ListadeUsuarios)
+            {
+                if (usuario.Nombre == nombre && usuario.Contrasena == contrasena)
+                {
+                    return usuario;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SistemaDeInventario/Login.cs
@@ -0,0 +1,57 @@
+﻿using SistemaDeInventario.BL;
+using SistemaDeInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeInventario
+{
+    public partial class Login : Form
+    {
+        UsuariosBL usuariosBL;
+
+        public Usuario UsuarioAutenticado { get; set; }
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var nombre = textBox1.Text;
+            var contrasena = textBox2.Text;
+
+            var usuarioAutenticado = _usuariosBL.Autenticar(nombre, contrasena);
+
+            if (usuarioAutenticado != null)
+            {
+                usuarioAutenticado = usuarioAutenticado;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o Contrasena Incorrecta");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void CargarDatos(UsuariosBL usuariosBL)
+        {
+            usuariosBL = usuariosBL;
+        }
+    }
+}
